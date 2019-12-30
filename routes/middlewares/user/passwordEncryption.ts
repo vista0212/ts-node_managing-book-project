ed764b30622231ee60a9410f5c22d3dc134ab906
kb4ek <kbaek1202@gmail.com>
2019-12-31 00:25:40 +0900

    feat: create password encryption API

---
+++ routes/middlewares/user/passwordEncryption.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import { randomBytes, pbkdf2Sync } from 'crypto';
+import User from '@Model/user.model';
+
+const passwordEncryption = (req: Request, res: Response, next: NextFunction) => {
+    const password: User['password'] = req.body.password;
+    const passwordKey: User['passwordKey'] = (res.locals.user && res.locals.user.passwordKey) || randomBytes(64).toString('base64');
+
+    const PASSWORD_ENCRYPTION_CONFIG = {
+        ITERATION: parseInt(process.env.PASSWORD_ENCRYPTION_ITERATION, 10),
+        KEY_LENGTH: parseInt(process.env.PASSWORD_ENCRYPTION_KEY_LENGTH, 10),
+        DIGEST: process.env.PASSWORD_ENCRYPTION_DIGEST,
+    };
+
+    const encryptionPassword: string = pbkdf2Sync(
+        password,
+        passwordKey,
+        PASSWORD_ENCRYPTION_CONFIG.ITERATION,
+        PASSWORD_ENCRYPTION_CONFIG.KEY_LENGTH,
+        PASSWORD_ENCRYPTION_CONFIG.DIGEST
+    ).toString('base64');
+
+    res.locals.temp = {
+        password: encryptionPassword,
+        passwordKey,
+    }
+
+    next();
+}
+
+export default passwordEncryption;
