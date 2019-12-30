ce2def5e9aaac81a2bf9910197935d9ea759d70d
kb4ek <kbaek1202@gmail.com>
2019-12-31 00:26:01 +0900

    feat: create issue jwt token API

---
+++ routes/middlewares/jwt/issueToken.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
+
+import User from '@Model/user.model';
+
+const issueToken = (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user;
+  const secretKey: string = process.env.ACCESS_TOKEN_SECRET;
+
+  const accessToken = jwt.sign({
+    pk: user.pk,
+  },
+  secretKey,
+  {
+    expiresIn: '1h',
+  });
+
+  res.json({
+    success: true,
+    data: {
+      userId: user.id,
+      userName: user.name,
+      access_token: accessToken,
+    },
+  });
+}
+
+export default issueToken;
