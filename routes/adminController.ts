5b6c88eaa132eb6540c0576ceea49b5d79559120
kb4ek <kbaek1202@gmail.com>
2020-01-02 21:56:46 +0900

    feat: change logic

--- routes/adminController.ts
+++ routes/adminController.ts
@@ -1,19 +1,26 @@
 import { Router } from 'express';
+
 import postBookValidation from '@Middleware/admin/book/post/_validation';
+import deleteBookValidation from '@Middleware/admin/book/delete/_validation';
 import checkValidation from '@Middleware/common/checkValidation';
+
 import verifyToken from '@Middleware/jwt/verifyToken';
 import getUserFromToken from '@Middleware/jwt/getUserFromToken';
 import checkAdmin from '@Middleware/admin/common/checkAdmin';
+
 import postBook from '@Middleware/admin/book/post/postBook';
+import deleteBook from '@Middleware/admin/book/delete/deleteBook';
 
 const router: Router = Router();
 
 router.use(verifyToken, getUserFromToken, checkAdmin);
 
 router.post('/book', postBookValidation);
+router.delete('/book', deleteBookValidation);
 
 router.use(checkValidation);
 
 router.post('/book', postBook);
+router.delete('/book', deleteBook);
 
 export default router;
--- routes/apiController.ts
+++ routes/apiController.ts
@@ -3,12 +3,13 @@ import { Router } from 'express';
 import userController from '@Controller/user.controller';
 import bookController from '@Controller/book.controller';
 import verifyToken from '@Middleware/jwt/verifyToken';
+import getUserFromToken from '@Middleware/jwt/getUserFromToken';
 
 const router: Router = Router();
 
 router.use('/user', userController);
 
-router.use(verifyToken)
+router.use(verifyToken, getUserFromToken);
 
 router.use('/book', bookController);
 
