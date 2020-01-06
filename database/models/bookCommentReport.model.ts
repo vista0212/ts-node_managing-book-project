63f53c34de6599aedf258fde4e3a2dbd76a87092
kb4ek <kbaek1202@gmail.com>
2020-01-06 23:57:12 +0900

    feat: update book comment report model

--- database/models/bookCommentReport.model.ts
+++ database/models/bookCommentReport.model.ts
@@ -1,14 +1,29 @@
-import { Model, DataTypes } from 'sequelize';
+import { Model, DataTypes, BelongsTo } from 'sequelize';
 
 import { sequelize } from '../index';
+import User from './user.model';
+import Book from './book.model';
+import BookComment from './bookComment.model';
 
 export default class BookCommentReport extends Model<BookCommentReport> {
+  public static associations: {
+    user: BelongsTo<BookCommentReport, User>;
+    book: BelongsTo<BookCommentReport, Book>;
+    bookComment: BelongsTo<BookCommentReport, BookComment>;
+  }
+
+  public user: User;
+  public book: Book;
+  public bookComment: BookComment;
 
   public pk: number;
   public user_pk: string;
   public book_pk: number;
   public comment_pk: number;
   public content: string;
+
+  public readonly createdAt: Date;
+  public readonly updatedAt: Date;
 };
 
 BookCommentReport.init({
